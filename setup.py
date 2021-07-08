6e6e3a3435a433cfc01df59f0a30306847141a44
rahul.bhaviri <rahul.bhaviri@mindtickle.com>
2021-07-08 17:40:58 +0530

    using graphQL client that supports headers

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r") as fh:
 
 setup(
     name='locust-graphql-client',
-    version='0.3.4',
+    version='0.0.2',
     packages=['locustgraphqlclient'],
-    url='https://github.com/rohitghatol/locust-graphql-client',
+    url='https://github.com/rahulnoobest/locust-graphql-client',
     license='Apache License 2.0',
-    author='rohitghatol',
+    author='rahulnoobest',
     author_email='',
     description='Locust GraphQL client GraphQL. This is a GraphQLLocust equivalent of HttpLocust',
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
-        'graphqlclient',
+        'graphqlclient@git+https://github.com/alaptseu/python-graphql-client.git@master',
         'locust'
     ]
 )
